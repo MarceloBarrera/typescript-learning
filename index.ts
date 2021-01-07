d13c87c49389fe3d1e831608dab5cd672c5d849e
“MarceloBarrera” <marbarrerach@hotmail.com>
2021-01-07 15:09:59 +0000

    Work with DOM elem in TS using Type Assertions

--- index.ts
+++ index.ts
@@ -7,3 +7,12 @@ function greet({greeting,name}:Salutation){
 const messaage:string = greet({greeting:"hi",name:"pepe"})
 
 //playing with typescript to see errors, trying parameters, optional parameters, destructuring objects, etc.
+
+const input = document.getElementById("input") as HTMLInputElement;
+input.autofocus = true;
+
+input.addEventListener("input", event => {
+    const inp = event.currentTarget as HTMLInputElement;
+    console.log(inp.value);
+    //console.log(event.currentTarget.value)
+})
