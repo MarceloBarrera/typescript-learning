e42305f3f7d73e75925f8bdf93b76d3e5b66ccad
“MarceloBarrera” <marbarrerach@hotmail.com>
2021-01-07 08:47:43 +0000

    notes an playing around with syntax errors

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("hello from TS")
+type Salutation={greeting:string, name:string};
+
+function greet({greeting,name}:Salutation){
+    return `${greeting}, ${name}`;
+}
+
+const messaage:string = greet({greeting:"hi",name:"pepe"})
+
+//playing with typescript to see errors, trying parameters, optional parameters, destructuring objects, etc.
