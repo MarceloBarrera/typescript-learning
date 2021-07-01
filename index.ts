49a2bcabc0bdc2e850d354f76def70997df2237c
Marcelo Barrera <barreram@emea.cshare.net>
2021-07-01 14:44:32 +0100

    more examples added

--- index.ts
+++ index.ts
@@ -53,3 +53,50 @@ const reversed = reverse(objects);
 reversed.push({name:'a'})
 
 // the cool thing about this is that you could not "push" an object {nana: 'a'} cause "nana" will be warn by TypeScript
+
+let value: any;
+
+value = true;             // OK
+value = 42;               // OK
+value = "Hello World";    // OK
+value = [];               // OK
+value = {};               // OK
+value = Math.random;      // OK
+value = null;             // OK
+value = undefined;        // OK
+value = new TypeError();  // OK
+value = Symbol("type");   // OK
+
+// let value: any;
+
+value.foo.bar;  // OK
+value.trim();   // OK
+value();        // OK
+new value();    // OK
+value[0][1];    // OK
+
+//////
+
+let valueFoo: unknown;
+
+valueFoo = true;             // OK
+valueFoo = 42;               // OK
+valueFoo = "Hello World";    // OK
+valueFoo = [];               // OK
+valueFoo = {};               // OK
+valueFoo = Math.random;      // OK
+valueFoo = null;             // OK
+valueFoo = undefined;        // OK
+valueFoo = new TypeError();  // OK
+valueFoo = Symbol("type");   // OK
+
+// let value: unknown;
+
+let value1: unknown = valueFoo;   // OK
+let value2: any = valueFoo;       // OK
+let value3: boolean = valueFoo;   // Error
+let value4: number = valueFoo;    // Error
+let value5: string = valueFoo;    // Error
+let value6: object = valueFoo;    // Error
+let value7: any[] = valueFoo;     // Error
+let value8: Function = valueFoo;  // Error
